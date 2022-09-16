d6f9de6d106dee15fc494d0c1b99a5595d8989d6
OverRam <yugini666@gmail.com>
2022-09-16 23:59:52 +0200

    Create a class and override the handleMethodArgumentNotValid method

---
+++ Account Service/task/src/account/errors/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+package account.errors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+
+        StringBuilder sb = new StringBuilder();
+        ex.getBindingResult().getAllErrors().forEach(e -> sb.append(e.getDefaultMessage()).append("; "));
+        sb.replace(sb.length() - 3, sb.length() - 1, "");
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        body.put("message", sb);
+        body.put("path", request.getDescription(false).replace("uri=", ""));
+        return new ResponseEntity<>(body, headers, status);
+    }
+}
