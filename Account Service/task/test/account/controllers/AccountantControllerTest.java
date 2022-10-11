52e38272d3603f66563880ee21d71f7d2c812830
OverRam <yugini666@gmail.com>
2022-10-11 22:04:28 +0200

    Tests

---
+++ Account Service/task/test/account/controllers/AccountantControllerTest.java
@@ -0,0 +1,21 @@
+package account.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountantControllerTest {
+
+    @Test
+    void uploadsPayrolls() {
+
+    }
+
+    @Test
+    void changeSalaryUser() {
+    }
+
+    @Test
+    void giveAccessToPayrollEmployee() {
+    }
+}
