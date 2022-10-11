4529bd295c287abffe3652ba7c4664dddb803632
OverRam <yugini666@gmail.com>
2022-10-11 22:07:52 +0200

    Delete var id

--- Account Service/task/src/account/model/DTO/UserViewResponseDTO.java
+++ Account Service/task/src/account/model/DTO/UserViewResponseDTO.java
@@ -7,13 +7,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class UserViewResponseDTO {
-    private Long id;
     private String name;
     private String lastname;
     private String email;
 
     public UserViewResponseDTO(User user) {
-        this.id = user.getId();
         this.name = user.getName();
         this.lastname = user.getLastname();
         this.email = user.getEmail();
